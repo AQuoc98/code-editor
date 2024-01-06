723f8f5f0299eb900b73ccdd9faecc2d7beb4812
Quốc Anh <quocanh@Quocs-MacBook-Pro.local>
2024-01-06 17:14:56 +0700

    update wezterm

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,11 +1,10 @@
 local wezterm = require 'wezterm'
 
 local config = {}
+if wezterm.config_builder then config = wezterm.config_builder() end
 
-if wezterm.config_builder then
-config = wezterm.config_builder()
-end
-
+-- Settings
 config.color_scheme = 'Badman'
+config.scrollback_lines = 3000
 
 return config
